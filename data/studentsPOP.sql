efcbabeee3717384c8890621d9afcad322b7504e
lrcannalongo <lrcannalongo@gmail.com>
2017-02-25 14:22:34 -0500

    students and ethnicity data
    randomized student data and some ethnicities to use

---
+++ data/studentsPOP.sql
@@ -0,0 +1,11 @@
+insert into student values 
+('123456789', 'John Harper', '2820 Pine St', 'Seattle', 'WA', 'MALE', TRUE, '1993-11-10', 'ENGLISH', 15, '236577', 3.1, FALSE, '01464163', 2011, 3.0, 140),
+('234567891', 'Janet Diedrich', '4225 Williams Ave', 'Fleming Island', 'FL', 'FEMALE', TRUE, '1994-03-15', 'GERMAN', 5, '461306', 3.5, FALSE, 'A9101894', 2012, 2.9, 170),
+('345678912', 'Jasmel Patel', '23 Race St', 'Philadelphia', 'PA', 'MALE', FALSE, '1992-07-03', 'INDIAN', 7, '216597', 2.9, FALSE, '340693000512', 2010, 3.2, 120),
+('456789123', 'Marcel Nehls', '114 Flower St', 'Austin', 'TX', 'MALE', TRUE, '1990-04-12', 'BRAZILIAN', 3, '439817', 3.9, FALSE, 'A0903543', 2008, 4.0, 1),
+('567891234', 'Marian Costa', '180 Raymond Dr', 'Sewell', 'NJ', 'FEMALE', TRUE, '1993-12-12', 'ITALIAN', 2, '214564', 2.6, FALSE, '340693000512', 2011, 2.5, 200),
+('678912345', 'Dennis Ren', 'PO Box 670', 'Toledo', 'OH', 'MALE', TRUE, '1991-02-07', 'JAPANESE', 7, '155052', 1.9, TRUE, '390466802621', 2009, 2.2, 140),
+('789123456', 'Elaine Price', '21 Hartford Ln', 'Denver', 'CO', NULL, TRUE, '1993-04-04', 'KENYAN', 8, '110608', 3.6, FALSE, 'A9101527', 2011, 3.1, 202),
+('891234567', 'Taylor Flynn', '1888 Columbia Blvd', 'Anaheim', 'CA', NULL, TRUE, '1989-09-07', NULL, 12, '110608', 3.4, FALSE, '61443001699', 2007, 2.8, 250),
+('912345678', 'Phillip Osterly', '12 Crestview Ct', 'Augusta', 'ME', 'MALE', TRUE, '1994-08-01', 'GERMAN', NULL, '161341', NULL, NULL, '00940135', NULL, NULL, NULL),
+('120934879', 'Cindi DeJesus', '412 Clarion St Apt B', 'Minneapolis', 'MN', 'FEMALE', TRUE, '1997-06-11', 'MEXICAN', 4, '175263', 2.7, NULL, '270028703631', NULL, 2.7, NULL)
